30711d05f77b94e8fe4ccf7638e3817a10bd6e20
rosangleijsteen <74179329+rosangleijsteen@users.noreply.github.com>
2021-01-07 13:14:32 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+    deNav = event.target.parentNode;
+    deNav.classList.toggle("toonMenu");
+}
